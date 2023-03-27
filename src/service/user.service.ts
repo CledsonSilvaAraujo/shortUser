5882e01c8ed2e91a91118b427cab0180277ebe23
CledsonSilvaAraujo <cledson_silva@id.uff.br>
2023-03-26 22:25:29 -0300

    feat: add shortlinks to userService

--- src/service/user.service.ts
+++ src/service/user.service.ts
@@ -1,13 +1,29 @@
+import ShortLink from '../models/ShortLink';
+import utils from '../util/utils';
 import { ObjectId } from 'mongodb';
-import  mongoose, { FilterQuery, UpdateQuery } from 'mongoose';
-import User, { IUser } from '../models/User';
+import  { FilterQuery, UpdateQuery } from 'mongoose';
+import User from '../models/User';
+import shortlinkService from './shortlink.service';
+
+const sizeOfShortLink = 8;
 
 const createUser = async ( name: string) => {
-  const user = new User({
-    name: name
-  });
-  await user.save();
-  return user;
+  try{
+    
+    if(name.length < 2 ) throw new Error("Name is to short");
+    const user = new User({
+      name: name
+    });
+
+    const shortLink = shortlinkService.createShortLink(user._id);
+    user.shortLinksId = shortLink._id;
+  
+    await shortLink.save();
+    await user.save();
+    return user;
+  }catch(error){
+    return error;
+  }
 };
 
 const findUser = async ( userId : string ) => {
@@ -28,16 +44,24 @@ const deleteUser = async ( userId : string ) => {
 
 const addLinkToUser =  async ( userId: string , link: string) => {
   const id  = userId.toString();
-  const filter :  FilterQuery<typeof User> = { _id: new ObjectId(id) };
-  const update = { $push: {"shortLink": link} }
-  return await User.findOneAndUpdate( filter, update).exec();
+  const user =  await User.findById(id).exec();
+  if(!user){ throw new Error( 'The User dosen\'t exist') }
+
+  const  _id  = user.shortLinksId;
+  const filter :  FilterQuery<typeof ShortLink> = { _id: new ObjectId(_id) };
+  const shortenLink: string = utils.generateString(sizeOfShortLink).trim();
+  const update = {
+      $push :  {"userShortLinks": [{ "shortenLink" : `${shortenLink}`, "link": `${link}`}]}
+  };
+  const updatedShortLink= await ShortLink.findOneAndUpdate( filter, update).exec()
+  return updatedShortLink;
+
 };
 
-const getUserShortLink =  async ( userId: string , linkId: number) => {
+const getUserShortLink =  async ( userId: string , shortLink: string) => {
   const id  = userId.toString();
-  const object = await User.find({_id: id} , { "shortLink" : { $slice : [linkId , linkId] } } ).exec()
-  const link = object[0].shortLink ?  object[0].shortLink[0] : null;
-  return link;
+  const object = await User.find({_id: id} , { "shortLinks" : { shortLink } } ).exec()
+  return object;
 };
 
 export default {
