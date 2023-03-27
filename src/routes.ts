ba7f5f187acf66e7b71717377b8afb64f777c534
CledsonSilvaAraujo <cledson_silva@id.uff.br>
2023-03-26 22:25:51 -0300

    feat: add shortlink to router

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,11 @@
 import { Router } from 'express';
 import userRouter from './routes/User';
+import shortLinkRouter from './routes/ShortLink';
 
 class Routes {
   static define(router: Router): Router {
     router.use('/user', userRouter);
-
+    router.use('/shortLink', shortLinkRouter);
     return router;
   }
 }
