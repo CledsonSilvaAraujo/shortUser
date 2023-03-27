c8a6adbb2922b468315c4dd332c304d1d78cb86b
CledsonSilvaAraujo <cledson_silva@id.uff.br>
2023-03-26 22:25:09 -0300

    test: from user

--- src/service/user.service.test.ts
+++ src/service/user.service.test.ts
@@ -13,45 +13,37 @@ afterAll( async () => {
 
 
 describe('should test basic features in user service',  () => {
+
   test('createUser and find update and delete user', async () => {
+    const userBefore = await userService.createUser('Joãozinho');
+    expect(userBefore).toBeTruthy();
+  });
+
+  test('updateUser', async () => {
     const userBefore = await userService.createUser('Joãozinho');
     const _id = userBefore._id.toString();
     await userService.updateUserById(_id, { name : 'Cledson'});
     const userAfter = await userService.findUser( _id );
-    await userService.deleteUser( _id );
-    const userAfterAll =  await userService.findUser( _id );
-
     expect(userBefore).toBeTruthy();
     expect(userAfter?.name).toBe("Cledson");
+  });
+
+  test('deleteUser', async () => {
+    const userBefore = await userService.createUser('Joãozinho');
+    const _id = userBefore._id.toString();
+    await userService.deleteUser( _id );
+    const userAfterAll =  await userService.findUser( _id );
+    expect(userBefore).toBeTruthy();
     expect(userAfterAll).toBeFalsy();
   });
 
-  //aqui eu quero fazer um teste usando nock
-  // test('if link its being associated with user', async () => {
-  //   const userInserted = await userService.createUser('Joãozinho');
-  //   const _id = userInserted._id.toString();
-  //   await userService.updateUserById(_id, { name : 'Cledson'});
-  //   const userAfter = await userService.findUser( _id );
-  //   await userService.deleteUser( _id );
-  //   const userAfterAll =  await userService.findUser( _id );
-
-  //   expect(userBefore).toBeTruthy();
-  //   expect(userAfter?.name).toBe("Cledson");
-  //   expect(userAfterAll).toBeFalsy();
-  // });
-
-})
-
-
-
-describe('create user',  () => {
-  test('if number of caracteres in user name is less then 2', async () => {
-   try{
-    const userBefore = await userService.createUser('Joãozinho');
-   }catch(err){
-    expect(err.message).toBe("TAL");
-   }
+  test.only('if number of caracteres in user name is less then 2', async () => {
+        const error = await userService.createUser('T');
+        expect(error.message).toBe("Name is to short");
+       
   });
-
+    
 
 })
+
+
